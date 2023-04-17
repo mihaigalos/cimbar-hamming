b02185e2bf1a6184610ba600f0a3e1e544182eb8
Mihai Galos <mihaigalos@gmail.com>
2023-04-17 18:44:55 +0200

    Tiles 5x5: Add tiles H - 8,19

--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
 import sys
 from generator import Generator
+from hamming import Hamming
+from default_tiles import *
 
-if len(sys.argv) > 1 and sys.argv[1] == "--no-default-tiles":
-    g = Generator(initial_tiles=[])
-else:
-    g = Generator()
-g.run()
+h = Hamming(DEFAULT_TILE_5x5)
+h.compute_distances()
+h.print()
+# if len(sys.argv) > 1 and sys.argv[1] == "--no-default-tiles":
+#     g = Generator(initial_tiles=[])
+# else:
+#     g = Generator()
+# g.run()
